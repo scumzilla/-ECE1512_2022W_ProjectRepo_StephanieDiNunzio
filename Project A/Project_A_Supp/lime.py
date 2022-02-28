0894b960e4cfb1eb6d4ccabf3f6d781f82e3d920
scumzilla <stephanie.dinunzio@mail.utoronto.ca>
2022-02-27 22:08:21 -0500

    some stuff

--- Project A/Project_A_Supp/lime.py
+++ Project A/Project_A_Supp/lime.py
@@ -8,8 +8,19 @@ import copy
 import sklearn
 import sklearn.metrics
 from sklearn.linear_model import LinearRegression
+import math
 
 np.random.seed(50)
+
+def perturb_img_1d(img, perturb, sec_size):
+    mask = np.zeros(img.shape)[0]
+    not_masked = np.where(perturb == 1)[0]
+    for i, val in enumerate(not_masked):
+        if val != 0:
+            for j in range(sec_size):
+                mask[sec_size*i + j] = 1
+    perturbed_img = img*mask[:,np.newaxis]
+    return perturbed_img
 
 def perturb_img(img, perturb, segs):
     mask = np.zeros(segs.shape)
@@ -21,7 +32,7 @@ def perturb_img(img, perturb, segs):
     return perturbed_img
 
 
-def LIME(img, model, label, num_perturb=300, kernel_w=0.25, num_feats=4):
+def LIME_img(img, model, label, num_perturb=300, kernel_w=0.25, num_feats=10):
     superpixels = skimage.segmentation.quickshift(img, kernel_size=4,max_dist=200, ratio=0.2)
     num_superpixels = np.unique(superpixels).shape[0]
     skimage.io.imshow(skimage.segmentation.mark_boundaries(img, superpixels))
@@ -42,3 +53,36 @@ def LIME(img, model, label, num_perturb=300, kernel_w=0.25, num_feats=4):
     mask = np.zeros(num_superpixels) 
     mask[top_feats]= True 
     skimage.io.imshow(perturb_img(img,mask,superpixels) )
+
+def LIME_1d(img, model, label, num_perturb=300, sec_size=4, kernel_w=0.25, num_feats=4):
+    num_superpixels = math.ceil(img.shape[1]/sec_size)
+    perturbs = np.random.binomial(1, 0.5, size=(num_perturb, num_superpixels))
+    preds = []
+    for i in perturbs:
+        perturbed_img = perturb_img_1d(img, i, sec_size)
+        pred = model.predict(perturbed_img)
+        preds.append(pred)
+    preds = np.array(preds)
+    orig_img = np.ones(num_superpixels)[np.newaxis,:]
+    dists = sklearn.metrics.pairwise_distances(perturbs, orig_img, metric='cosine').ravel()
+    weights = np.sqrt(np.exp(-(dists**2)/kernel_w**2))
+    lime_model = LinearRegression()
+    lime_model.fit(perturbs, preds[:,:,label], weights)
+    c = lime_model.coef_[0]
+    top_feats = np.argsort(c)[-num_feats:]
+    mask_superpixel = np.zeros(num_superpixels) 
+    mask_superpixel[top_feats]= True 
+    mask = np.zeros(img.shape[1])
+    for i, val in enumerate(mask_superpixel):
+        if val != 0:
+            for j in range(sec_size):
+                mask[sec_size*i + j] = 1
+    perturbed_img = img*mask[:,np.newaxis]
+    
+    c_exp = np.zeros(img.shape[1])
+    for i, c_i in enumerate(c):
+        for j in range(sec_size):
+                if c_i > 0:
+                    c_exp[sec_size*i + j] = c_i
+
+    return c_exp, perturbed_img[0] 
